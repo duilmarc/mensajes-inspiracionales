a84011f84e1da890a340e6b8cf7768aa1b7c52b4
duilmarc <franco.chp@gmail.com>
2020-03-21 00:23:59 -0500

    añadiendo nuevas rutas al indice

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const server = express();
 
 const {PORT} = require('./config/index.js');
+// ROUTES
+const { HomeRoutes, QuoteRoutes} = require('./routes');
+const { NotFoundMiddleware } = require('./middlewares');
 
+// Implementacion del middleware para manejo de los archivos publicos
+server.use(express.static('./public'));
+// Transformar peticiones post a formato json
+server.use(express.json());
 
+server.use('/quotes',QuoteRoutes);
+server.use('/', HomeRoutes);
+
+server.use(NotFoundMiddleware);
+
+// ESCUCHA DEL SERVIDOR
 server.listen(PORT, () => {
     console.log( ` Application running on PORT ${PORT}` );
 });
